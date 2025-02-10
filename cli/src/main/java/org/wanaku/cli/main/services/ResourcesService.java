7a15d8dc380f19b60d9fe31496759bc83307f101
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-02-10 15:28:18 +0100

    Added resources list command

--- cli/src/main/java/org/wanaku/cli/main/services/ResourcesService.java
+++ cli/src/main/java/org/wanaku/cli/main/services/ResourcesService.java
@@ -1,5 +1,8 @@
 package org.wanaku.cli.main.services;
 
+import java.util.List;
+
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -15,4 +18,9 @@ public interface ResourcesService {
     @Produces({ MediaType.APPLICATION_JSON})
     @Path("/expose")
     Response expose(ResourceReference resourceReference);
+
+    @Path("/list")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    List<ResourceReference> list();
 }
--- cli/src/main/java/org/wanaku/cli/main/support/PrettyPrinter.java
+++ cli/src/main/java/org/wanaku/cli/main/support/PrettyPrinter.java
@@ -2,11 +2,12 @@ package org.wanaku.cli.main.support;
 
 import java.util.List;
 
+import org.wanaku.api.types.ResourceReference;
 import org.wanaku.api.types.ToolReference;
 
 public class PrettyPrinter {
 
-    public static void printParseableTools(final ToolReference toolReference) {
+    public static void printParseable(final ToolReference toolReference) {
         System.out.printf("%-15s => %-15s => %-30s    %n",
                 toolReference.getName(), toolReference.getType(), toolReference.getUri());
     }
@@ -20,7 +21,26 @@ public class PrettyPrinter {
                 "Name", "Type", "URI");
 
         for (ToolReference toolReference : list) {
-            printParseableTools(toolReference);
+            printParseable(toolReference);
+        }
+    }
+
+    public static void printParseable(final ResourceReference resourceReference) {
+        System.out.printf("%-20s => %-15s => %-30s    %s%n",
+                resourceReference.getName(), resourceReference.getType(), resourceReference.getLocation(),
+                resourceReference.getDescription());
+    }
+
+    /**
+     * Prints a list of packages
+     * @param list the list of packages
+     */
+    public static void printResources(final List<ResourceReference> list) {
+        System.out.printf("%-20s    %-15s    %-30s    %s%n",
+                "Name", "Type", "Location", "Description");
+
+        for (ResourceReference packageInfo : list) {
+            printParseable(packageInfo);
         }
     }
 
