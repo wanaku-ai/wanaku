95d48478713724306b951217badadbc5869c0b79
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-02-27 11:55:28 +0100

    Block by default as resource acquisition is typically blocking

--- archetypes/wanaku-provider-archetype/src/main/resources/archetype-resources/src/main/java/ResourceService.java
+++ archetypes/wanaku-provider-archetype/src/main/resources/archetype-resources/src/main/java/ResourceService.java
@@ -23,6 +23,7 @@ import ai.wanaku.core.exchange.InquireReply;
 import ai.wanaku.core.exchange.InquireRequest;
 import ai.wanaku.core.exchange.Inquirer;
 import io.quarkus.grpc.GrpcService;
+import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Uni;
 import ai.wanaku.core.exchange.ResourceAcquirer;
 import ai.wanaku.core.exchange.ResourceAcquirerDelegate;
@@ -36,6 +37,7 @@ public class ResourceService implements ResourceAcquirer, Inquirer {
     ResourceAcquirerDelegate delegate;
 
     @Override
+    @Blocking
     public Uni<ResourceReply> resourceAcquire(ResourceRequest request) {
         return Uni.createFrom().item(() -> delegate.acquire(request));
     }
