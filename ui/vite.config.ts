45ec4010b9f686f8f6656bd44a6bdf68d6062904
Ricardo M. <lordrip@gmail.com>
2025-02-28 16:07:35 +0100

    feat(UI): Add quinoa
    Currently, the UI is built separately from the project.
    
    This commit adds the quarkus quinoa extension to build the UI as part of
    the project build.
    
    How to use:
    Launch the project in dev mode, and as part of the `wanaku-router`
    model, the UI will be started in `dev` mode.
    
    When building for production, the UI will be built and added to the
    final artifact.
    
    fix: https://github.com/wanaku-ai/wanaku/issues/76

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,22 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-const outDir = './dist';
+const outDir = "./dist";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    VITE_API_URL:
+      process.env.VITE_API_URL ??
+      JSON.stringify("http://localhost:8080/api/v1"),
+  },
   build: {
     outDir,
     sourcemap: true,
     emptyOutDir: true,
   },
-  base: './',
+  base: "./",
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler',
+        api: "modern-compiler",
       },
     },
   },
-})
+});
