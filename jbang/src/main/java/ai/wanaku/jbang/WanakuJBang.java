abb92ee7decf0c7fd86b537a4879b5608850fdb9
Lukas Lowinger <llowinge@redhat.com>
2025-03-30 20:24:08 +0200

    [closes #215] Add JBang support

---
+++ jbang/src/main/java/ai/wanaku/jbang/WanakuJBang.java
@@ -0,0 +1,13 @@
+package ai.wanaku.jbang;
+
+//JAVA 17+
+//REPOS mavencentral
+// Without following config, the @Inject doesn't know about eg. WanakuCliConfig
+//Q:CONFIG quarkus.index-dependency.wanaku.group-id=ai.wanaku
+//Q:CONFIG quarkus.index-dependency.wanaku.artifact-id=cli
+
+import ai.wanaku.cli.main.Main;
+
+// Extends just to ensure, that deletion of ai.wanaku.cli.main.Main will also break JBang script
+public class WanakuJBang extends Main {
+}
