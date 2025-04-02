67bcc03e7509beb6903bc163ef9d7a340c32f5fa
Otavio R. Piske <angusyoung@gmail.com>
2025-04-02 09:08:09 +0200

    Removed unused profile-specific resolver

--- routers/wanaku-router/src/main/java/ai/wanaku/routers/ToolsProvider.java
+++ routers/wanaku-router/src/main/java/ai/wanaku/routers/ToolsProvider.java
@@ -1,26 +1,19 @@
 package ai.wanaku.routers;
 
+import ai.wanaku.core.mcp.common.resolvers.ToolsResolver;
+import ai.wanaku.core.mcp.common.resolvers.util.NoopToolsResolver;
 import ai.wanaku.core.mcp.providers.ServiceRegistry;
 import ai.wanaku.core.util.VersionHelper;
-import java.io.File;
-
+import ai.wanaku.routers.proxies.tools.InvokerProxy;
+import ai.wanaku.routers.resolvers.WanakuToolsResolver;
+import io.quarkus.arc.DefaultBean;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Instance;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
-
-import ai.wanaku.core.mcp.common.resolvers.ToolsResolver;
-import ai.wanaku.core.mcp.common.resolvers.util.NoopToolsResolver;
-import ai.wanaku.routers.config.WanakuRouterConfig;
-import ai.wanaku.routers.proxies.tools.InvokerProxy;
-import ai.wanaku.routers.resolvers.WanakuToolsResolver;
-import io.quarkus.arc.DefaultBean;
-import io.quarkus.arc.profile.IfBuildProfile;
 import org.jboss.logging.Logger;
 import picocli.CommandLine;
-
-import static ai.wanaku.core.mcp.common.resolvers.Resolver.DEFAULT_TOOLS_INDEX_FILE_NAME;
 
 /**
  * A provider for tools resolvers
@@ -40,17 +33,6 @@ public class ToolsProvider extends AbstractProvider<ToolsResolver> {
     @PostConstruct
     void init() {
         serviceRegistry = serviceRegistryInstance.get();
-    }
-
-    @Produces
-    @IfBuildProfile(anyOf = {"dev", "test"})
-    public ToolsResolver devResolver() {
-        if (parseResult.isUsageHelpRequested() || parseResult.isVersionHelpRequested()) {
-            return new NoopToolsResolver();
-        }
-
-        LOG.infof("Wanaku version %s is starting", VersionHelper.VERSION);
-        return new WanakuToolsResolver(new InvokerProxy(serviceRegistry));
     }
 
     @Produces
