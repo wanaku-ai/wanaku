6c0c43f5ff0d84140798cc3e440d59654a38e1d3
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-04-10 15:43:59 +0200

    Adjust SQS
    - Ajust the request queue so that the request URI with the queueOrArn is not dependent on the user
    - Document

--- services/tools/wanaku-tool-service-sqs/src/main/java/ai/wanaku/tool/sqs/SQSClient.java
+++ services/tools/wanaku-tool-service-sqs/src/main/java/ai/wanaku/tool/sqs/SQSClient.java
@@ -2,16 +2,13 @@ package ai.wanaku.tool.sqs;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
-import ai.wanaku.core.exchange.ParsedToolInvokeRequest;
 import ai.wanaku.core.exchange.ToolInvokeRequest;
 import ai.wanaku.core.services.tool.Client;
-
+import java.util.Map;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ConsumerTemplate;
 import org.apache.camel.ProducerTemplate;
 import org.jboss.logging.Logger;
-
-import java.util.Map;
 
 @ApplicationScoped
 public class SQSClient implements Client {
@@ -34,18 +31,19 @@ public class SQSClient implements Client {
         String accessKey = serviceConfigurationsMap.get("accessKey");
         String secretKey = serviceConfigurationsMap.get("secretKey");
         String region = serviceConfigurationsMap.get("region");
-        ParsedToolInvokeRequest parsedRequest = ParsedToolInvokeRequest.parseRequest(request);
-        String requestUri = String.format("%s?accessKey=RAW(%s)&secretKey=RAW(%s)&region=%s", parsedRequest.uri(), accessKey, secretKey, region);
 
-        String queueNameOrArn = serviceConfigurationsMap.get("queueNameOrArn");
-        String responseUri = String.format("aws2-sqs://%s?accessKey=RAW(%s)&secretKey=RAW(%s)&region=%s", queueNameOrArn, accessKey, secretKey, region);
+        String requestQueue = serviceConfigurationsMap.get("requestQueue");
+        String requestUri = String.format("aws2-sqs:%s?accessKey=RAW(%s)&secretKey=RAW(%s)&region=%s", requestQueue, accessKey, secretKey, region);
+
+        String responseQueue = serviceConfigurationsMap.get("responseQueue");
+        String responseUri = String.format("aws2-sqs:%s?accessKey=RAW(%s)&secretKey=RAW(%s)&region=%s", responseQueue, accessKey, secretKey, region);
 
         LOG.infof("Invoking tool at URI: %s", requestUri);
         try {
             producer.start();
             consumer.start();
 
-            producer.sendBody(requestUri, parsedRequest.body());
+            producer.sendBody(requestUri, request.getBody());
 
             LOG.infof("Waiting for reply at at URI: %s", responseUri);
             return consumer.receiveBody(responseUri);
