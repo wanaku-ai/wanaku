468acc8d91fa8388e91a9677f79b96266063c6da
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-04-23 12:03:02 +0200

    Fixes archetypes for service-defined arguments
    This is related to issue #245

--- tests/archetype-tests/test-archetypes.sh
+++ tests/archetype-tests/test-archetypes.sh
@@ -1,7 +1,14 @@
-currentDir=$(pwd)
-cliJar=${currentDir}/cli/target/quarkus-app/quarkus-run.jar
+localDir="$(readlink -f $0)"
+installDir="$(dirname "${localDir}")"/../..
 
-cd "${currentDir}"/cli/target
+cliDir=${installDir}/cli/target/quarkus-app/
+cliJar=${cliDir}/quarkus-run.jar
+
+if [[ -d build ]] ; then
+  rm -r build
+fi
+
+mkdir build && cd build
 
 java -jar ${cliJar} services create resource --name test
 if [[ ! -d wanaku-provider-test ]] ; then
@@ -33,5 +40,3 @@ if [[ $? -eq 0 ]] ; then
   echo "The code does not compile"
   exit 2
 fi
-
-cd "$currentDir"
