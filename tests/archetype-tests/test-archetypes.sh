63d05e47ceaf63ee426ecb75cdabffff55aa789d
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-04-24 09:11:59 +0200

    Simplify launching the router, resource providers and tools as services in the background

--- tests/archetype-tests/test-archetypes.sh
+++ tests/archetype-tests/test-archetypes.sh
@@ -1,6 +1,8 @@
 localDir="$(readlink -f $0)"
 installDir="$(dirname "${localDir}")"/../..
 
+workDir=$(pwd)
+
 cliDir=${installDir}/cli/target/quarkus-app/
 cliJar=${cliDir}/quarkus-run.jar
 
@@ -19,13 +21,13 @@ else
   cd wanaku-provider-test
 fi
 
-mvn clean package
-if [[ $? -eq 0 ]] ; then
+mvn -Pdist clean package
+if [[ $? -ne 0 ]] ; then
   echo "The code does not compile"
   exit 2
 fi
 
-cd "${currentDir}"/cli/target
+cd "${workDir}"/build
 
 java -jar ${cliJar} services create tool --name test
 if [[ ! -d wanaku-tool-service-test ]] ; then
@@ -35,8 +37,11 @@ else
   cd wanaku-tool-service-test
 fi
 
-mvn clean package
-if [[ $? -eq 0 ]] ; then
+mvn -Pdist clean package
+if [[ $? -ne 0 ]] ; then
   echo "The code does not compile"
   exit 2
 fi
+
+
+cd "${workDir}"
