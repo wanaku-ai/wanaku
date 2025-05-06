85e16d6bdd1dbb500c5507ad8206d56922d11174
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-05-06 11:02:17 +0200

    Minimize visibility of direct access to index
    So we can start cleaning it up a bit

--- core/core-util/src/main/java/ai/wanaku/core/util/IndexHelper.java
+++ core/core-util/src/main/java/ai/wanaku/core/util/IndexHelper.java
@@ -1,36 +1,24 @@
 package ai.wanaku.core.util;
 
+import ai.wanaku.api.types.ResourceReference;
+import ai.wanaku.api.types.ToolReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import ai.wanaku.api.types.ResourceReference;
-import ai.wanaku.api.types.ToolReference;
 
 /**
  * Helper class for dealing with the resources file
  */
 public class IndexHelper {
 
-    private IndexHelper() {}
-
-    /**
-     * Load an index
-     * @param indexFile
-     * @return
-     * @throws Exception
-     */
-    private static <T, Y> Map<T, Y> loadIndex(File indexFile, Class<T> clazzT, Class<Y> clazzY) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(indexFile,
-                objectMapper.getTypeFactory().constructMapType(Map.class, clazzT, clazzY));
+    private IndexHelper() {
     }
 
     /**
      * Load an index
+     *
      * @param indexFile
      * @return
      * @throws Exception
@@ -43,6 +31,7 @@ public class IndexHelper {
 
     /**
      * Load an index
+     *
      * @param indexURL
      * @return
      * @throws Exception
@@ -53,13 +42,7 @@ public class IndexHelper {
                 objectMapper.getTypeFactory().constructCollectionType(List.class, clazz));
     }
 
-    /**
-     * Saves an index of resources to a file
-     * @param indexFile
-     * @param resourceReferences
-     * @throws IOException
-     */
-    public static <T> void saveIndex(File indexFile, List<T> resourceReferences) throws IOException {
+    private static <T> void saveIndex(File indexFile, List<T> resourceReferences) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         objectMapper
@@ -69,16 +52,18 @@ public class IndexHelper {
 
     /**
      * Load an index of resources
+     *
      * @param indexFile
      * @return
      * @throws Exception
      */
-    public static List<ResourceReference> loadResourcesIndex(File indexFile) throws Exception {
+    static List<ResourceReference> loadResourcesIndex(File indexFile) throws Exception {
         return loadIndex(indexFile, ResourceReference.class);
     }
 
     /**
      * Saves an index to a file
+     *
      * @param indexFile
      * @param resourceReferences
      * @throws IOException
@@ -89,6 +74,7 @@ public class IndexHelper {
 
     /**
      * Load an index of tools
+     *
      * @param indexFile
      * @return
      * @throws Exception
@@ -99,6 +85,7 @@ public class IndexHelper {
 
     /**
      * Load an index of tools
+     *
      * @param indexURL
      * @return
      * @throws Exception
@@ -109,47 +96,12 @@ public class IndexHelper {
 
     /**
      * Saves an index of tools to a file
+     *
      * @param indexFile
      * @param toolReferences
      * @throws IOException
      */
     public static void saveToolsIndex(File indexFile, List<ToolReference> toolReferences) throws IOException {
         saveIndex(indexFile, toolReferences);
-    }
-
-    /**
-     * Saves an index of targets to a file
-     * @param indexFile
-     * @param map
-     * @throws IOException
-     */
-    public static <T> void saveIndex(File indexFile, Map<T, ?> map) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        objectMapper
-                .writerWithDefaultPrettyPrinter()
-                .writeValue(indexFile, map);
-    }
-
-    /**
-     * Saves an index of targets to a file
-     * @param indexFile
-     * @param targetsMap
-     * @throws IOException
-     */
-    public static void saveTargetsIndex(File indexFile, Map<String, ?> targetsMap) throws IOException {
-        saveIndex(indexFile, targetsMap);
-    }
-
-
-
-    /**
-     * Load an index of targets and their configurations
-     * @param indexFile
-     * @return
-     * @throws Exception
-     */
-    public static <T> Map<String, T> loadTargetsIndex(File indexFile, Class<T> serviceClass) throws Exception {
-        return loadIndex(indexFile, String.class, serviceClass);
     }
 }
