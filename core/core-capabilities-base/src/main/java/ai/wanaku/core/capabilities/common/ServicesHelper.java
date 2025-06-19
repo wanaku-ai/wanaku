ca2c397c00d5604bdc9eeaa5b76c60336697adda
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-06-19 11:03:33 +0200

    Cleanup duplicated code

--- core/core-capabilities-base/src/main/java/ai/wanaku/core/capabilities/common/ServicesHelper.java
+++ core/core-capabilities-base/src/main/java/ai/wanaku/core/capabilities/common/ServicesHelper.java
@@ -1,7 +1,18 @@
 package ai.wanaku.core.capabilities.common;
 
+import ai.wanaku.api.types.providers.ServiceTarget;
+import ai.wanaku.core.capabilities.config.WanakuServiceConfig;
+import ai.wanaku.core.capabilities.discovery.DefaultRegistrationManager;
+import ai.wanaku.core.capabilities.discovery.RegistrationManager;
 import ai.wanaku.core.exchange.InquireReply;
 import ai.wanaku.core.exchange.InvocationDelegate;
+import ai.wanaku.core.service.discovery.client.DiscoveryService;
+import ai.wanaku.core.service.discovery.util.DiscoveryUtil;
+import io.quarkus.rest.client.reactive.QuarkusRestClientBuilder;
+import java.io.File;
+import java.net.URI;
+import java.util.Map;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.logging.Logger;
 
 public class ServicesHelper {
@@ -32,5 +43,40 @@ public class ServicesHelper {
         return InquireReply.newBuilder()
                 .putAllProperties(delegate.properties())
                 .build();
+    }
+
+    public static RegistrationManager newRegistrationManager(WanakuServiceConfig config, Map<String, String> serviceConfigurations) {
+        LOG.infof("Using registration service at %s", config.registration().uri());
+        DiscoveryService discoveryService = QuarkusRestClientBuilder.newBuilder()
+                .baseUri(URI.create(config.registration().uri()))
+                .build(DiscoveryService.class);
+
+
+        String service = config.name();
+        ServiceTarget serviceTarget = newServiceTarget(config, service, serviceConfigurations);
+
+        int retries = config.registration().retries();
+        int waitSeconds = config.registration().retryWaitSeconds();
+
+        final String serviceHome =
+                config.serviceHome().replace("${user.home}", System.getProperty("user.home"))
+                        + File.separator
+                        + config.name();
+
+        return new DefaultRegistrationManager(discoveryService, serviceTarget, retries, waitSeconds, serviceHome);
+    }
+
+    private static ServiceTarget newServiceTarget(WanakuServiceConfig config, String service, Map<String, String> configurations) {
+        String portStr = ConfigProvider.getConfig().getConfigValue("quarkus.grpc.server.port").getValue();
+        final int port = Integer.parseInt(portStr);
+
+
+        String address = config.registration().announceAddress();
+        if ("auto".equals(address)) {
+            LOG.infof("Using announce address %s ", address);
+            address = DiscoveryUtil.resolveRegistrationAddress();
+        }
+
+        return ServiceTarget.provider(service, address, port, configurations);
     }
 }
