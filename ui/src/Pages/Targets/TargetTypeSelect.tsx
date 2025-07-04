460f42d06400671a3defb0f4c90d1246cace0a48
Croway <federico.mariani.1990@gmail.com>
2025-07-04 16:05:53 +0200

    Dynamically load resources and tools types, via capabilities/targets api

---
+++ ui/src/Pages/Targets/TargetTypeSelect.tsx
@@ -0,0 +1,41 @@
+import { Select, SelectItem } from "@carbon/react";
+import React, { useEffect, useState } from "react";
+import { ServiceTarget } from "../../models";
+
+interface TargetTypeSelectProps {
+  value: string;
+  onChange: (value: string) => void;
+  apiCall: () => Promise<any>;
+}
+
+export const TargetTypeSelect: React.FC<TargetTypeSelectProps> = ({
+  value,
+  onChange,
+  apiCall
+}) => {
+  const [targetTypes, settargetTypes] = useState<ServiceTarget[]>([]);
+
+  useEffect(() => {
+    apiCall().then((result) => {
+      settargetTypes(result.data.data as ServiceTarget[]);
+    });
+  }, []);
+
+  return (
+    <Select
+      id="target-type"
+      labelText="Type"
+      defaultValue="file"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {targetTypes.map((targetType) => (
+        <SelectItem
+          key={targetType.id}
+          value={targetType.service}
+          text={targetType.service || ""}
+        />
+      ))}
+    </Select>
+  );
+};
