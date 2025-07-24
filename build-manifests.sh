e71cc9e3ea48dffb800eaddd9dfa933dbc9e2191
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-07-24 13:47:56 +0200

    Stable versions should use their own branches as the manifest tag

--- build-manifests.sh
+++ build-manifests.sh
@@ -13,8 +13,19 @@ for manifest in $manifests ; do
   echo "Pulling ${x864Image}"
   podman pull ${x864Image}
 
-  echo "Creating the manifest ${manifest}"
-  podman manifest create ${manifest} ${aarch64Image} ${x864Image}
-  podman manifest push --all ${manifest}
+  if [[ "${branch}" == "main" ]] ; then
+    echo "Creating the manifest ${manifest}"
+    podman rmi -f ${manifest} || true
+    podman manifest create ${manifest} ${aarch64Image} ${x864Image}
+
+    podman manifest push --all ${manifest}
+  else
+    stableManifest=$(echo "${manifest}" | sed "s/:[^:]*$/:${branch}/")
+
+    echo "Creating the manifest ${stableManifest}"
+    podman rmi -f ${stableManifest} || true
+    podman manifest create ${stableManifest} ${aarch64Image} ${x864Image}
+    podman manifest push --all ${stableManifest}
+  fi
 
 done
