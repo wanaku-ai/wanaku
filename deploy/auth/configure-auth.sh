1b4f34a4265b2c64c260de393bb2dcc62fd96744
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-09-25 12:45:12 +0200

    Get the keycloak host automatically

--- deploy/auth/configure-auth.sh
+++ deploy/auth/configure-auth.sh
@@ -1,6 +1,8 @@
 wanaku_service_client_id=wanaku-service
 wanaku_service_client_uuid=102929ec-264a-4acb-8111-970d62a112fb
 
+WANAKU_KEYCLOAK_HOST=$(oc get routes keycloak -o json  | jq -r .spec.host)
+
 # Get the admin token
 TOKEN=$(curl -s -d 'client_id=admin-cli' -d 'username=admin' -d "password=$WANAKU_KEYCLOAK_PASS" -d 'grant_type=password' "http://${WANAKU_KEYCLOAK_HOST}/realms/master/protocol/openid-connect/token" | jq -r '.access_token')
 
