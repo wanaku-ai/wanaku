7d871cc300d516a7efe0845d181e20b1609be90d
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-10-13 14:33:56 +0200

    Add support for Streamable HTTP in MCP forwards
    This fixes issue #569

--- core/core-mcp-client/src/main/java/ai/wanaku/core/mcp/client/ClientUtil.java
+++ core/core-mcp-client/src/main/java/ai/wanaku/core/mcp/client/ClientUtil.java
@@ -4,15 +4,25 @@ import dev.langchain4j.mcp.client.DefaultMcpClient;
 import dev.langchain4j.mcp.client.McpClient;
 import dev.langchain4j.mcp.client.transport.McpTransport;
 import dev.langchain4j.mcp.client.transport.http.HttpMcpTransport;
+import dev.langchain4j.mcp.client.transport.http.StreamableHttpMcpTransport;
 
 public class ClientUtil {
 
     public static McpClient createClient(String address) {
-        McpTransport transport = new HttpMcpTransport.Builder()
-                .sseUrl(address)
-                .logRequests(true)
-                .logResponses(true)
-                .build();
+        McpTransport transport;
+        if (address.endsWith("sse") || address.endsWith("sse/")) {
+            transport = new HttpMcpTransport.Builder()
+                    .sseUrl(address)
+                    .logRequests(true)
+                    .logResponses(true)
+                    .build();
+        } else {
+            transport = new StreamableHttpMcpTransport.Builder()
+                    .url(address)
+                    .logRequests(true)
+                    .logResponses(true)
+                    .build();
+        }
 
         return new DefaultMcpClient.Builder().transport(transport).build();
     }
