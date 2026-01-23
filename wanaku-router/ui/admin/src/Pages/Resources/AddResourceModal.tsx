5c6b155836eac008e11aeec12d1a188c9aba3b85
Radovan Netuka <rnetuka@li-f098124c-3165-11b2-a85c-bf544180b79e.ibm.com>
2026-01-23 15:06:16 +0100

    [#527][UI] Add Parameters Array Support to Resource Forms

--- wanaku-router/ui/admin/src/Pages/Resources/AddResourceModal.tsx
+++ wanaku-router/ui/admin/src/Pages/Resources/AddResourceModal.tsx
@@ -2,14 +2,21 @@ import {
   Modal,
   TextInput,
   Select,
-  SelectItem, ComboBox,
+  SelectItem,
+  ComboBox,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs
 } from "@carbon/react";
 import React, { useEffect, useState } from "react";
-import { Namespace, ResourceReference } from "../../models";
+import { Namespace, Param, ResourceReference } from "../../models";
 import { commonMimeTypes, commonMimeTypesMapping } from "../../constants/mimeTypes.ts";
 import { listNamespaces } from "../../hooks/api/use-namespaces";
 import { useCapabilities } from "../../hooks/api/use-capabilities";
 import { TargetTypeSelect } from "../Targets/TargetTypeSelect";
+import { ParametersTable } from "./ParametersTable.tsx";
 
 interface AddResourceModalProps {
   onRequestClose: () => void;
@@ -27,6 +34,7 @@ export const AddResourceModal: React.FC<AddResourceModalProps> = ({
   const [mimeType, setMimeType] = useState("");
   const [fetchedData, setFetchedData] = useState<Namespace[]>([]);
   const [selectedNamespace, setSelectedNamespace] = useState('');
+  const [params, setParams] = useState<Param[]>([]);
   const { listManagementResources } = useCapabilities();
   
   useEffect(() => {
@@ -47,6 +55,7 @@ export const AddResourceModal: React.FC<AddResourceModalProps> = ({
       type: resourceType,
       mimeType,
       namespace: selectedNamespace,
+      params: nonEmptyParameters()
     });
   };
 
@@ -61,6 +70,22 @@ export const AddResourceModal: React.FC<AddResourceModalProps> = ({
     }
   }
 
+  function newParameter() {
+    const temp = [...params]
+    temp.push({name: '', value: ''})
+    setParams(temp)
+  }
+
+  function removeParameter(i: number) {
+    const temp = [...params]
+    temp.splice(i, 1)
+    setParams(temp)
+  }
+
+  function nonEmptyParameters() {
+    return params.filter(parameter => parameter.name !== '')
+  }
+
   return (
     <Modal
       open={true}
@@ -70,67 +95,86 @@ export const AddResourceModal: React.FC<AddResourceModalProps> = ({
       onRequestClose={onRequestClose}
       onRequestSubmit={handleSubmit}
     >
-      <TextInput
-        id="resource-name"
-        labelText="Resource Name"
-        placeholder="e.g. example-resource"
-        value={resourceName}
-        onChange={(e) => setResourceName(e.target.value)}
-      />
-      <TextInput
-        id="resource-description"
-        labelText="Description"
-        placeholder="e.g. Description of the resource"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <TextInput
-        id="resource-location"
-        labelText="Location"
-        placeholder="e.g. /path/to/resource"
-        value={location}
-        onChange={(e) => {
-          const location = e.target.value
-          setLocation(location)
-          autoDetectMimeType(location)
-        }}
-      />
-      <TargetTypeSelect
-        value={resourceType}
-        onChange={setResourceType}
-        apiCall={listManagementResources}
-      />
-      <ComboBox
-          id="resource-mime-type"
-          titleText="MIME Type"
-          placeholder="Select or enter MIME type"
-          items={commonMimeTypes}
-          selectedItem={mimeType}
-          onChange={(e) => {
-            let value = e.selectedItem
-            if (value === undefined || value === null) {
-              value = ""
-            }
-            setMimeType(value)
-          }}
-          helperText="Content type of the resource (optional)"
-      />
-      <Select
-        id="namespace"
-        labelText="Select a Namespace"
-        helperText="Choose a Namespace from the list"
-        value={selectedNamespace}
-        onChange={handleSelectionChange}
-      >
-        <SelectItem text="Choose an option" value="" />
-        {fetchedData.map((namespace) => (
-          <SelectItem
-            id={namespace.id}
-            text={namespace.path || "default"}
-            value={namespace.id}
-          />
-        ))}
-      </Select>
+      <Tabs>
+        <TabList>
+          <Tab>Overview</Tab>
+          <Tab>Parameters</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <TextInput
+              id="resource-name"
+              labelText="Resource Name"
+              placeholder="e.g. example-resource"
+              value={resourceName}
+              onChange={(e) => setResourceName(e.target.value)}
+            />
+            <TextInput
+              id="resource-description"
+              labelText="Description"
+              placeholder="e.g. Description of the resource"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <TextInput
+              id="resource-location"
+              labelText="Location"
+              placeholder="e.g. /path/to/resource"
+              value={location}
+              onChange={(e) => {
+                const location = e.target.value
+                setLocation(location)
+                autoDetectMimeType(location)
+              }}
+            />
+            <TargetTypeSelect
+              value={resourceType}
+              onChange={setResourceType}
+              apiCall={listManagementResources}
+            />
+            <ComboBox
+                id="resource-mime-type"
+                titleText="MIME Type"
+                placeholder="Select or enter MIME type"
+                items={commonMimeTypes}
+                selectedItem={mimeType}
+                onChange={(e) => {
+                  let value = e.selectedItem
+                  if (value === undefined || value === null) {
+                    value = ""
+                  }
+                  setMimeType(value)
+                }}
+                helperText="Content type of the resource (optional)"
+            />
+            <Select
+              id="namespace"
+              labelText="Select a Namespace"
+              helperText="Choose a Namespace from the list"
+              value={selectedNamespace}
+              onChange={handleSelectionChange}
+            >
+              <SelectItem text="Choose an option" value="" />
+              {fetchedData.map((namespace) => (
+                <SelectItem
+                  id={namespace.id}
+                  text={namespace.path || "default"}
+                  value={namespace.id}
+                />
+              ))}
+            </Select>
+          </TabPanel>
+          <TabPanel>
+            <ParametersTable
+                parameters={params}
+                onAdd={newParameter}
+                onSetName={(i, name) => params[i].name = name}
+                onSetValue={(i, value) => params[i].value = value}
+                onDelete={removeParameter}
+            />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Modal>
   );
 };
