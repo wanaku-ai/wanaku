e5ec5642f03d83c2c69e96125b6a6e54a2c69a5e
Otavio Rodolfo Piske <angusyoung@gmail.com>
2026-03-01 15:05:51 +0100

    Fix incorrect launch scripts templates

--- archetypes/wanaku-provider-archetype/src/main/resources/archetype-resources/src/main/scripts/launcher.bat
+++ archetypes/wanaku-provider-archetype/src/main/resources/archetype-resources/src/main/scripts/launcher.bat
@@ -1,9 +1,7 @@
 @echo off
 
-set SDM_HOME=%~dp0\..
-
 @REM Set
-if %OS%=="Windows_NT" @setlocal
-if %OS%=="WINNT" @setlocal
+if "%OS%"=="Windows_NT" @setlocal
+if "%OS%"=="WINNT" @setlocal
 
 @java -jar quarkus-run.jar %*
