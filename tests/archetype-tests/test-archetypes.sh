a20547df13222b129c7dc5bc5984b74f85955096
Otavio Rodolfo Piske <angusyoung@gmail.com>
2026-04-07 10:57:50 +0200

    Test camel capabilities in the test-archetypes.sh script

--- tests/archetype-tests/test-archetypes.sh
+++ tests/archetype-tests/test-archetypes.sh
@@ -43,5 +43,38 @@ if [[ $? -ne 0 ]] ; then
   exit 2
 fi
 
+cd "${workDir}"/build
+
+java -jar ${cliJar} capabilities create resource --name test-camel --type camel
+if [[ ! -d wanaku-provider-test-camel ]] ; then
+  echo "The generated directory is wrong for camel resource"
+  ls -1
+  exit 1
+else
+  cd wanaku-provider-test-camel
+fi
+
+mvn -Pdist clean package
+if [[ $? -ne 0 ]] ; then
+  echo "The camel resource code does not compile"
+  exit 2
+fi
+
+cd "${workDir}"/build
+
+java -jar ${cliJar} capabilities create tool --name test-camel --type camel
+if [[ ! -d wanaku-tool-service-test-camel ]] ; then
+  echo "The generated directory is wrong for camel tool"
+  exit 1
+else
+  cd wanaku-tool-service-test-camel
+fi
+
+mvn -Pdist clean package
+if [[ $? -ne 0 ]] ; then
+  echo "The camel tool code does not compile"
+  exit 2
+fi
+
 
 cd "${workDir}"
