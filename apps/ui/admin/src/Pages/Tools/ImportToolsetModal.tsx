2cc15cb740fcb4b881d7a0ac3a6de1bd051317b6
Radovan Netuka <rnetuka@li-f098124c-3165-11b2-a85c-bf544180b79e.ibm.com>
2026-04-29 13:41:38 +0200

    Import toolset modal overhaul

--- apps/ui/admin/src/Pages/Tools/ImportToolsetModal.tsx
+++ apps/ui/admin/src/Pages/Tools/ImportToolsetModal.tsx
@@ -1,11 +1,17 @@
 import {
+  Column,
+  ContentSwitcher,
+  Grid,
   Modal,
   Stack,
+  Switch,
   TextArea,
   TextInput
 } from "@carbon/react"
-import {useState} from "react"
+import React, {useRef, useState} from "react"
 import {ToolReference} from "../../models"
+import {ToolParserError, Tools} from "./tools"
+import {ImportToolsetTable} from "./ImportToolsetTable"
 
 
 interface ImportToolsetModalProps {
@@ -15,30 +21,94 @@ interface ImportToolsetModalProps {
 
 export const ImportToolsetModal: React.FC<ImportToolsetModalProps> = ({ onSubmit, onCancel }) => {
   
-  const [toolsetUrl, setToolsetUrl] = useState("")
-  const [toolsetJson, setToolsetJson] = useState("")
+  const FINAL_STEP = 1
+  const VIEW_MODE_TABLE = "table"
+  const VIEW_MODE_JSON = "json"
+  
+  const [toolsetUrl, setToolsetUrl] = useState<string>()
+  const [invalidUrl, setInvalidUrl] = useState(false)
+  const [toolset, setToolset] = useState<ToolReference[]>([])
+  const [toolsetJson, setToolsetJson] = useState<string>()
   const [invalidJson, setInvalidJson] = useState(false)
+  const [invalidJsonText, setInvalidJsonText] = useState<string>()
+  const [step, setStep] = useState(0)
+  const [contentSwitcherEnabled, setContentSwitcherEnabled] = useState(true)
+  const [viewMode, setViewMode] = useState(VIEW_MODE_TABLE)
+  const selectedTools = useRef<ToolReference[]>([])
+
   
+  function setSelectedTools(tools: ToolReference[]) {
+    selectedTools.current = tools
+    setToolsetJson(tools.length > 0 ? Tools.stringify(tools) : undefined)
+  }
   
-  async function handleFetchToolset() {
-    if (toolsetUrl) {
-      const response = await fetch(toolsetUrl)
-      if (response.ok) {
-        const tools = await response.json()
-        setToolsetJson(JSON.stringify(tools, null, 2))
+  function primaryButtonText(): string {
+    return step === FINAL_STEP ? "Import" : "Next"
+  }
+  
+  function primaryButtonDisabled(): boolean {
+    return step === FINAL_STEP && !toolsetJson
+  }
+  
+  function isToolsetUrlEmpty(): boolean {
+    return !toolsetUrl
+  }
+  
+  function isToolsetUrlValid(): boolean {
+    try {
+      new URL(toolsetUrl!)
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+  
+  async function fetchToolset() {
+    const response = await fetch(toolsetUrl!)
+    if (response.ok) {
+      const tools = await response.json()
+      setToolset(tools)
+      setSelectedTools(tools)
+    } else {
+      throw new Error(`Error fetching toolset from ${toolsetUrl}: ${response.status}`)
+    }
+  }
+  
+  async function handlePrimaryButton() {
+    if (step === FINAL_STEP) {
+      handleSubmit()
+    } else {
+      if (isToolsetUrlEmpty()) {
+        // skip fetching toolset, user can copy-paste in the next step
+        setStep(step + 1)
+        setViewMode(VIEW_MODE_JSON)
+        setContentSwitcherEnabled(false)
+      } else if (isToolsetUrlValid()) {
+        // try to fetch toolset and proceed to next step
+        try {
+          await fetchToolset()
+          setStep(step + 1)
+        } catch (error) {
+          console.error(error)
+        }
       } else {
-        console.error(`Error fetching toolset from ${toolsetUrl}`)
+        // URL is invalid
+        setInvalidUrl(true)
       }
     }
   }
   
-  async function handleSubmit() {
+  function handleSubmit() {
     try {
-      const tools = JSON.parse(toolsetJson)
-      setInvalidJson(false)
+      const tools: ToolReference[] = Tools.parse(toolsetJson!)
       onSubmit(tools)
     } catch (error) {
-      setInvalidJson(true)
+      if (error instanceof SyntaxError || error instanceof ToolParserError) {
+        setInvalidJson(true)
+        setInvalidJsonText(error.message)
+      } else {
+        console.error(error)
+      }
     }
   }
   
@@ -46,35 +116,75 @@ export const ImportToolsetModal: React.FC<ImportToolsetModalProps> = ({ onSubmit
     <Modal
       open={true}
       modalHeading="Import Toolset"
-      primaryButtonText="Import"
+      primaryButtonText={primaryButtonText()}
+      primaryButtonDisabled={primaryButtonDisabled()}
       secondaryButtonText="Cancel"
-      onRequestSubmit={handleSubmit}
+      onRequestSubmit={handlePrimaryButton}
       onRequestClose={onCancel}
     >
       <Stack gap={7}>
-        <TextInput
-          id="toolset-url"
-          labelText="Fetch from Toolset URL"
-          placeholder="Enter the URL of the toolset JSON"
-          value={toolsetUrl}
-          onChange={(event) => {
-            setToolsetUrl(event.target.value)
-          }}
-          onBlur={handleFetchToolset}
-        />
-        <TextArea
-          id="toolset-json"
-          labelText="Toolset JSON"
-          placeholder="Paste your JSON array here"
-          rows={10}
-          required
-          value={toolsetJson}
-          onChange={(event) => {
-            setToolsetJson(event.target.value)
-          }}
-          invalid={invalidJson}
-          invalidText="Invalid JSON"
-        />
+        {step === 0 && (
+          <TextInput
+            id="toolset-url"
+            labelText="Fetch from Toolset URL"
+            placeholder="Enter the URL of the toolset JSON"
+            helperText="You can skip this step and enter the data manually"
+            invalid={invalidUrl}
+            invalidText="Invalid URL"
+            onChange={(event) => {
+              setToolsetUrl(event.target.value)
+              setInvalidUrl(false)
+            }}
+          />
+        )}
+        {step === 1 && (
+          <>
+            {contentSwitcherEnabled && (
+            <Grid>
+              <Column></Column>
+              <Column lg={{ span: 4, offset: 12 }} md={{ span: 4, offset: 4 }} sm={4}>
+                <ContentSwitcher
+                  onChange={({ name }) => { setViewMode(name as string) }}
+                  selectedIndex={viewMode === VIEW_MODE_TABLE ? 0 : 1}
+                >
+                  <Switch
+                    name={VIEW_MODE_TABLE}
+                    text="List view"
+                  />
+                  <Switch
+                    name={VIEW_MODE_JSON}
+                    text="Json"
+                  />
+                </ContentSwitcher>
+              </Column>
+            </Grid>
+            )}
+            {viewMode === VIEW_MODE_TABLE && (
+              <ImportToolsetTable
+                tools={toolset}
+                selectedTools={selectedTools.current}
+                onSelectionChange={(tools: ToolReference[]) => {
+                  setSelectedTools(tools)
+                }}
+              />
+            )}
+            {viewMode === VIEW_MODE_JSON && (
+              <TextArea
+                id="toolset-json"
+                labelText="Toolset JSON"
+                placeholder="Paste your JSON array here"
+                rows={10}
+                required
+                value={toolsetJson}
+                invalid={invalidJson}
+                invalidText={invalidJsonText}
+                onChange={(event) => {
+                  setToolsetJson(event.target.value)
+                }}
+              />
+            )}
+          </>
+        )}
       </Stack>
     </Modal>
   )
---
+++ apps/ui/admin/src/Pages/Tools/tools.ts
@@ -0,0 +1,45 @@
+import {ToolReference} from "../../models"
+
+
+export class ToolParserError extends Error {
+  
+  constructor(message: string) {
+    super(message)
+  }
+  
+}
+
+function convertToTool(json): ToolReference {
+  const tool: ToolReference = {}
+  if (!json.name) {
+    throw new ToolParserError("Tool must have a name")
+  }
+  if (typeof json.name !== "string") {
+    throw new ToolParserError("Tool name must be a string")
+  }
+  for (const key in json) {
+    tool[key] = json[key]
+  }
+  return tool
+}
+
+export class Tools {
+  
+  static stringify(tools: ToolReference | ToolReference[]): string {
+    return JSON.stringify(tools, null, 2)
+  }
+  
+  static parse(string: string): ToolReference[] {
+    const json: unknown = JSON.parse(string)
+    if (Array.isArray(json)) {
+      const tools: ToolReference[] = []
+      for (const object of json) {
+        tools.push(convertToTool(object))
+      }
+      return tools
+    } else {
+      return [convertToTool(json)]
+    }
+  }
+  
+}
