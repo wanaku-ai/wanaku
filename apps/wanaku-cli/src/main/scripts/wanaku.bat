dd4e6df9edd587fca56840a77e15c8a6ce65cbca
Otavio Rodolfo Piske <angusyoung@gmail.com>
2026-06-03 11:57:37 +0200

    Fix #1236: Unify CLI launch scripts to support both JAR and native binary

--- apps/wanaku-cli/src/main/scripts/wanaku.bat
+++ apps/wanaku-cli/src/main/scripts/wanaku.bat
@@ -1,9 +1,12 @@
 @echo off
 
-set SDM_HOME=%~dp0\..
-
-@REM Set
 if %OS%=="Windows_NT" @setlocal
 if %OS%=="WINNT" @setlocal
 
+if exist "%~dp0wanaku-cli.exe" (
+    "%~dp0wanaku-cli.exe" %*
+    exit /b %ERRORLEVEL%
+)
+
+set SDM_HOME=%~dp0\..
 @java -jar quarkus-run.jar %*
